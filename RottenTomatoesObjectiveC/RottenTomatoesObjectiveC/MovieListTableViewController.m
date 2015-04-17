9d7ae57ede0c548532fab7f76848b049784e7ee6
Jeffrey Bergier <jeffburg@jeffburg.com>
2015-04-16 20:21:48 -0700

    finished rotten tomatoes api section

--- RottenTomatoesObjectiveC/RottenTomatoesObjectiveC/MovieListTableViewController.m
+++ RottenTomatoesObjectiveC/RottenTomatoesObjectiveC/MovieListTableViewController.m
@@ -11,6 +11,8 @@
 
 @interface MovieListTableViewController()
 
+@property (strong, nonatomic) NSArray *moviesArray;
+
 @end
 
 @implementation MovieListTableViewController
@@ -18,15 +20,49 @@
 -(void)viewDidLoad {
     [super viewDidLoad];
     
+    // Download rotten tomatoes file
+    NSString *apiKey = @"qe43pmsb84evcmyj43gbe7j8";
+    NSString *urlString = [NSString stringWithFormat:@"http://api.rottentomatoes.com/api/public/v1.0/lists/movies/upcoming.json?apikey=%@", apiKey];
+    NSURL *url = [[NSURL alloc] initWithString:urlString];
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+            NSHTTPURLResponse *httpResponse = ((NSHTTPURLResponse*) response);
+            switch (httpResponse.statusCode) {
+                case 200: {
+                    NSDictionary *json = [NSJSONSerialization JSONObjectWithData: data options: NSJSONReadingAllowFragments error: nil];
+                    NSArray *moviesArray = json[@"movies"];
+                    if (moviesArray) {
+                        // Grab the main queue because NSURLSession can callback on any
+                        // queue and we're touching non-atomic properties and the UI
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            self.moviesArray = moviesArray;
+                            [self.tableView reloadData];
+                        });
+                    }
+                    break;
+                }
+                default: {
+                    NSLog(@"Error downloading file: Response code %d", httpResponse.statusCode); // Handle Errors here
+                    break;
+                }
+            }
+        }
+    }];
+    [task resume];
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 5;
+    return self.moviesArray.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     MovieTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MovieCell"];
-    cell.movieTitleLabel.text = [NSString stringWithFormat:@"This is Cell Number %d", indexPath.row];
+    NSDictionary *movie = self.moviesArray[indexPath.row];
+    NSString *movieTitle = movie[@"title"];
+    NSString *movieDescription = movie[@"synopsis"];
+    
+    cell.movieTitleLabel.text = movieTitle;
+    cell.movieDescriptionLabel.text = movieDescription;
     return cell;
 }
 
--- RottenTomatoesSwift/RottenTomatoesSwift/MovieListTableViewController.swift
+++ RottenTomatoesSwift/RottenTomatoesSwift/MovieListTableViewController.swift
@@ -10,18 +10,50 @@ import UIKit
 
 class MovieListTableViewController: UITableViewController {
     
+    var moviesArray: [NSDictionary]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Download rotten tomatoes file
+        let apiKey = "qe43pmsb84evcmyj43gbe7j8"
+        let url: NSURL! = NSURL(string: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/upcoming.json?apikey=\(apiKey)")
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: {(downloadedData, response, error) in
+            if let httpResponse = response as? NSHTTPURLResponse {
+                switch httpResponse.statusCode {
+                case 200:
+                    if let data = downloadedData,
+                        let json = NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments, error: nil) as? NSDictionary,
+                        let moviesArray = json["movies"] as? [NSDictionary] {
+                            // Grab the main queue because NSURLSession can callback on any 
+                            // queue and we're touching non-atomic properties and the UI
+                            dispatch_async(dispatch_get_main_queue()) {
+                                self.moviesArray = moviesArray
+                                self.tableView.reloadData()
+                            }
+                    }
+                default:
+                    NSLog("Error downloading file: Response code \(httpResponse.statusCode)") // handle errors here
+                }
+            }
+        })
+        task.resume()
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return self.moviesArray?.count ?? 0
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell") as? MovieTableViewCell
-        cell?.movieTitleLabel.text = "This is cell number \(indexPath.row)"
+        if let moviesArray = self.moviesArray {
+            let movie = moviesArray[indexPath.row]
+            let movieTitle = movie["title"] as? String ?? "Title Not Found"
+            let movieDescription = movie["synopsis"] as? String ?? "Synopsys Not Found"
+            
+            cell?.movieTitleLabel.text = movieTitle
+            cell?.movieDescriptionLabel.text = movieDescription
+        }
         return cell!
     }
     
